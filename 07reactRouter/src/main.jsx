2cc3cee39a1f6e0f6161e4e8d58675cc227422e6
Gyan Prakash <gyan07prakash@gmail.com>
2026-07-12 23:58:26 +0530

    Set up React Router and project routing structure

---
+++ 07reactRouter/src/main.jsx
@@ -0,0 +1,35 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import {RouterProvider, createBrowserRouter} from 'react-router-dom'
+import Contact from './components/Contact/Contact.jsx'
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>,
+    children: [
+      {
+        path:"",
+        element: <Home />
+      },
+      {
+        path:"about",
+        element: <About />
+      },
+      {
+        path:"contact",
+        element: <Contact />
+      }
+    
+    ]
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router}/>
+  </StrictMode>,
+)
